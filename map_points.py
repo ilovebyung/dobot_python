99ef67fb1ef1d6f80bc2c9d7dcb06ab8fc5dfcf4
BYUNG <45321943+ilovebyung@users.noreply.github.com>
2023-11-23 23:18:35 +0900

    NearestNDInterpolator
    It maps a camera coordinate to a mapped robot coordinate.

--- map_points.py
+++ map_points.py
@@ -1,25 +1,34 @@
 import numpy as np
-from scipy.interpolate import interp2d
+from scipy.interpolate import NearestNDInterpolator
 
-# Define source and target points
-source_points = np.array([[60, 150], [270, 150], [480, 150], [62, 360], [270, 360], [480, 355]])
-target_points = np.array([[343, 97], [340, 0], [342, -100], [244, 97], [244, 0], [244, -100]])
+# Define the source and target points
+source_points = np.array([
+    [60, 150],
+    [270, 150],
+    [480, 150],
+    [62, 360],
+    [270, 360],
+    [480, 355]
+])
 
-# Extract x and y coordinates from source and target points
-x_source, y_source = source_points[:, 0], source_points[:, 1]
-x_target, y_target = target_points[:, 0], target_points[:, 1]
+target_points = np.array([
+    [343, 97],
+    [340, 0],
+    [342, -100],
+    [244, 97],
+    [244, 0],
+    [244, -100]
+])
 
-# Create the interpolation function
-x_f = interp2d(x_source, y_source, x_target, kind='linear')
-y_f = interp2d(x_source, y_source, y_target, kind='linear')
+# Create the NearestNDInterpolator object
+interpolator = NearestNDInterpolator(source_points, target_points)
 
-# Define the mapping function using the interpolation functions
-def map_points(x, y):
-    return x_f(x, y)[0], y_f(x, y)[0]
+# Define the query points
+query_points = np.array([
+    [62, 360]
+])
 
-# Test the mapping function with some example points
-test_point = [150, 150]
-mapped_point = map_points(*test_point)
+# Interpolate the query points
+interpolated_values = interpolator(query_points)
 
-print(f"Original point: {test_point}")
-print(f"Mapped point: {mapped_point}")
+print(interpolated_values)
